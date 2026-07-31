c9b51fe2abde4c33adee720c35ec0ca732ffd40e
Sowshil Dowpati <sowshild@gmail.com>
2026-07-31 21:47:07 +0530

    Add: is-anagram - submission-4

---
+++ Data Structures & Algorithms/is-anagram/submission-4.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int n=s.length(), m=t.length();
+        if(n!=m){
+            return false;
+        }
+        int freq[]=new int[26];
+        for(int i=0; i<s.length(); i++){
+            freq[s.charAt(i)-'a']++;
+        }
+        for(int j=0; j<t.length(); j++){
+            freq[t.charAt(j)-'a']--;
+        }
+        for(int i=0; i<freq.length; i++){
+            if(freq[i]!=0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
