2a37b60e934a1b61af07a932a76d82135e0a2a4e
Sowshil Dowpati <sowshild@gmail.com>
2026-09-10 14:03:07 +0530

    Add: decode-string - submission-1

---
+++ Data Structures & Algorithms/decode-string/submission-1.java
@@ -0,0 +1,31 @@
+class Solution {
+    public String decodeString(String s) {
+        Deque<Character> st=new ArrayDeque<>();
+        for(int i=0; i<s.length(); i++){
+            if(s.charAt(i)!=']'){
+                st.push(s.charAt(i));
+            }
+            else{
+                StringBuilder sub=new StringBuilder();
+                while(st.peek()!='['){
+                    sub.insert(0,st.pop());
+                }
+                st.pop();
+                StringBuilder k=new StringBuilder();
+                while(!st.isEmpty() && Character.isDigit(st.peek())){
+                    k.insert(0,st.pop());
+                }
+                int count=Integer.parseInt(k.toString());
+                String repStr=sub.toString().repeat(count);
+                for(int j=0; j<repStr.length(); j++){
+                    st.push(repStr.charAt(j));
+                }
+            }
+        }
+        StringBuilder res=new StringBuilder();
+        while(!st.isEmpty()){
+            res.insert(0,st.pop());
+        }
+        return res.toString();
+    }
+}
